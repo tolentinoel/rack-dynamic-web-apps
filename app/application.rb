44c79c972a4233eb11bac6f041bfdf26587c1e7e
tolentinoel <rckrleyn@gmail.com>
2020-10-25 12:43:14 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -22,10 +22,12 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
 
+    # the .write method is a rack method. using this instead of puts statement
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
 
+    #conditional statement to draw random number from the range/variables above. \n is another line
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
     else
